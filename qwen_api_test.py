9971cd1df6fc6bf44d79cf37d82dfff33d2b40df
YueZhengMeng <804570270@qq.com>
2025-02-17 03:22:13 +0800

    Qwen API接口测试

---
+++ qwen_api_test.py
@@ -0,0 +1,40 @@
+import os
+from openai import OpenAI
+
+try:
+    client = OpenAI(
+        # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
+        api_key=os.getenv("DASHSCOPE_API_KEY"),
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    )
+
+    # 查询可用的模型列表
+    models = client.models.list()
+    print(models)
+
+    completion = client.chat.completions.create(
+        model="qwen-plus",  # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', 'content': '你是谁？'}
+            ]
+    )
+    print(completion.choices[0].message.content)
+except Exception as e:
+    print(f"错误信息：{e}")
+    print("请参考文档：https://help.aliyun.com/zh/model-studio/developer-reference/error-code")
+
+
+client = OpenAI(
+    api_key=os.getenv("DASHSCOPE_API_KEY"),  # 如果您没有配置环境变量，请在此处用您的API Key进行替换
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"  # 百炼服务的base_url
+)
+
+completion = client.embeddings.create(
+    model="text-embedding-v3",
+    input=['风急天高猿啸哀', '渚清沙白鸟飞回', '无边落木萧萧下', '不尽长江滚滚来'],
+    dimensions=1024,
+    encoding_format="float"
+)
+
+print(completion.model_dump_json())
